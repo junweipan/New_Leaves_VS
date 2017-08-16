92af14cadd8120c22bd407cdb862b452382e117d
Daniel T <toro.duque@gmail.com>
2017-08-17 00:32:02 +0100

    HTML and CSS of theme added
    HTML and CSS of theme added. JS have not been added yet!

--- New_Leaves/New_Leaves/App_Start/BundleConfig.cs
+++ New_Leaves/New_Leaves/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace New_Leaves
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css",
+                      "~/Content/style.css"));
         }
     }
 }
