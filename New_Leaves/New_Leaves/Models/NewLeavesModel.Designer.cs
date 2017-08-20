84806daba1cd5df61e50762678a55e2f11b60f80
timleeshen <lishen9211@gmail.com>
2017-08-20 13:29:55 +1000

    tim

--- New_Leaves/New_Leaves/Models/NewLeavesModel.Designer.cs
+++ New_Leaves/New_Leaves/Models/NewLeavesModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\GitHub\New_Leaves_VS\New_Leaves\New_Leaves\Models\NewLeavesModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Downloads\New_Leaves_VS\New_Leaves\New_Leaves\Models\NewLeavesModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
