24d1e5f677bc82b5ac01aff49622c5098545165c
timleeshen <lishen9211@gmail.com>
2017-08-22 00:10:22 +1000

    add password encryption method

---
+++ New_Leaves/New_Leaves/Models/Extended/Donor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace New_Leaves.Models
+
+{
+    public partial class Donor
+    {
+    }
+}
