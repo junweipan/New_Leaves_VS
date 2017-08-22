5d59726279671d3062bbec3c0716ae3f69ad27ac
timleeshen <lishen9211@gmail.com>
2017-08-22 14:29:20 +1000

    database donor table attribute modification

--- New_Leaves/New_Leaves/Models/Extended/Donor.cs
+++ New_Leaves/New_Leaves/Models/Extended/Donor.cs
@@ -7,7 +7,41 @@ using System.ComponentModel.DataAnnotations;
 namespace New_Leaves.Models
 
 {
-    public partial class Donor
+    [MetadataType(typeof(DonorMetadata))]
+    public partial class Domor
     {
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class DonorMetadata
+    {
+        [Display(Name = "First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email ID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailID { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm password and password do not match")]
+        public string ConfirmPassword { get; set; }
+
     }
 }
