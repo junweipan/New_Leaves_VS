7f8323108fff5dde877f74b22cd615c579814e94
junweipan <panjunweide@gmail.com>
2017-08-28 13:13:00 +1000

    recreate the project

---
+++ New_Leaves/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace New_Leaves.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult IndexLogin()
+        {
+            return View("IndexLogin");
+
+        }
+
+        public ActionResult RefugeeIndex()
+        {
+            return View("RefugeeIndex");
+
+        }
+
+        public ActionResult Index()
+        {
+            return View("Index");
+
+        }
+        public ActionResult Team()
+           
+        {
+           
+
+            return View("team");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
