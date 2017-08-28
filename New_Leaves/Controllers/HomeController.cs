ddf320b51d4a1abfc6fe17597edc4dbb532432c0
junweipan <panjunweide@gmail.com>
2017-08-28 13:27:16 +1000

    See refugee own Info

--- New_Leaves/Controllers/HomeController.cs
+++ New_Leaves/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using New_Leaves.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,7 +10,22 @@ namespace New_Leaves.Controllers
 {
     public class HomeController : Controller
     {
-       
+        private icontest2Entities db = new icontest2Entities();
+        //Get refugee by email
+        public ActionResult Details(String email)
+        {
+            if (email == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Refugee refugee = db.Refugee.SingleOrDefault(r => r.Email == email);
+            // User myUser = myDBContext.Users.SingleOrDefault(user => user.Username == username);
+            if (refugee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(refugee);
+        }
         public ActionResult IndexLogin()
         {
             return View("IndexLogin");
