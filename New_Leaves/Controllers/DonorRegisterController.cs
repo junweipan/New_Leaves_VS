9f4f48f3d8ad8d8fbf7b3f4fb0ac2584482a5924
junweipan <panjunweide@gmail.com>
2017-09-06 22:44:56 +1000

    donate function 50%

--- New_Leaves/Controllers/DonorRegisterController.cs
+++ New_Leaves/Controllers/DonorRegisterController.cs
@@ -44,8 +44,8 @@ namespace New_Leaves.Controllers
                 #endregion
 
                 #region  Password Hashing 
-                donor.Password = Crypto.Hash(donor.Password);
-                donor.ConfirmPassword = Crypto.Hash(donor.ConfirmPassword); //
+               // donor.Password = Crypto.Hash(donor.Password);
+                //donor.ConfirmPassword = Crypto.Hash(donor.ConfirmPassword); //
                 #endregion
                 donor.IsEmailVerified = false;
 
@@ -116,7 +116,8 @@ namespace New_Leaves.Controllers
                 var v = dc.Donor.Where(a => a.Email == login.Email).FirstOrDefault();
                 if (v != null)
                 {
-                    if (string.Compare(Crypto.Hash(login.Password), v.Password) == 0)
+                    // if (string.Compare(Crypto.Hash(login.Password), v.Password) == 0)
+                    if (string.Compare(login.Password, v.Password) == 0)
                     {
                         int timeout = login.RememberMe ? 525600 : 20;
                         var ticket = new FormsAuthenticationTicket(login.Email, login.RememberMe, timeout);
@@ -133,7 +134,7 @@ namespace New_Leaves.Controllers
                         }
                         else
                         {
-                            return RedirectToAction("Index", "Home");
+                            return RedirectToAction("DonorIndex", "Home");
                         }
                     }
                     else
