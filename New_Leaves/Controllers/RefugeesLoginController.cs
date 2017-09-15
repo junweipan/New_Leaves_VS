7200bff14651f2ffc0fb213b6408111882359528
junweipan <panjunweide@gmail.com>
2017-09-15 14:00:09 +1000

    recover daniel version

--- New_Leaves/Controllers/RefugeesLoginController.cs
+++ New_Leaves/Controllers/RefugeesLoginController.cs
@@ -36,7 +36,8 @@ namespace New_Leaves.Controllers
                 var v = dc.Refugee.Where(a => a.AuthorityCode == login.AuthorityCode).FirstOrDefault();
                 if (v != null)
                 {
-                    if (string.Compare((login.Password), v.Password) == 0)
+                    if (string.Compare(Crypto.Hash(login.Password), v.Password) == 0)
+                       
                     {   
                         int timeout = login.RememberMe ? 525600 : 20;
                         var ticket = new FormsAuthenticationTicket(login.AuthorityCode, login.RememberMe, timeout);
